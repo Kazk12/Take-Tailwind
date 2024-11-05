170f4e5438bc8c4151cd4adb876945a41b41a49f
Kazk12 <kazuko43KO@gmail.com>
2024-11-05 16:54:52 +0100

    ADD:5 Sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["index.html"],
   theme: {
-    extend: {},
+    extend: {
+
+      backgroundImage : {
+        'bg-masthead': "url('../Take-assets/masthead.jpg')",
+        'bg-lg': "url('../Take-assets/box-bg-lg.jpg')",
+        'bg-box': "url('../Take-assets/box-bg.jpg')",
+        'full':"url('../Take-assets/full-image.jpg')",
+      },
+      fontFamily: {
+        Roboto: ["Roboto", "sans-serif"],
+        Rokkitt: ["Rokkitt", "sans-serif"],
+      },
+
+    },
   },
   plugins: [],
 }
-
